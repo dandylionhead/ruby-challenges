eebc342ec9ff74c9370b3dc0927ccc0331d6fb05
Ellen Grover <ellengrover@Ellens-iMac.local>
2016-02-27 17:12:14 -0900

    My first commit

---
+++ symbol_hash.rb
@@ -0,0 +1,10 @@
+doctor_1 = { companion: "Susan", description: "older with long hair"}
+doctor_4 = { companion: "K-9", description: "curly hair, long scarf"}
+doctor_9 = { companion: "Rose", description: "big ears and big smile"}
+doctor_10 = { companion: "Donna", description: "pinstripe suit and long coat"}
+doctor_11 = { companion: "Amy and Rory", description: "fez and a bow tie"}
+doctor_12 = { companion: "Clara", description: "skinny and Scottish"}
+
+best = doctor_4[:companion]
+
+puts "#{best} is the best Doctor Who companion"
